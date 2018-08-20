597d123302ed20992900b7c9db4eac00abf3d020
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2018-08-20 12:24:27 +0200

    Add org-templates
    Signed-off-by: Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>

---
+++ lisp/org-templates.el
@@ -0,0 +1,12 @@
+(add-to-list 'org-structure-template-alist
+             '("J" "#+LaTeX_CLASS: xe-org-article
+#+LATEX_HEADER: \\usepackage[ngerman]{babel}
+#+LATEX_HEADER: \\usepackage{color, colortbl}
+#+LATEX_HEADER: \\definecolor{headClr}{rgb}{.9,.9,.9}
+#+LATEX_HEADER: \\definecolor{ferienClr}{rgb}{1,1,.75}
+#+TITLE:     
+#+AUTHOR:    
+#+LANGUAGE:  de
+#+OPTIONS:   toc:nil
+
+*@@latex:\\textsf{Gruppe:}@@*  "))
